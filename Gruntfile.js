91d1d074dae76ce340b0c3b709721413f4c12106
Simone Busoli <simone.busoli@gmail.com>
2014-04-21 22:56:08 +0200

    Make credentials repo an optional dependency to be able to build on Travis Simplify source bundle name generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,11 +9,16 @@
 'use strict';
 
 module.exports = function (grunt) {
-  var exec = require('child_process').exec,
-      credentials = require('grunt-awsebtdeploy-credentials'),
-      fs = require('fs');
+  var fs = require('fs'),
+      Zip = require('adm-zip'),
+      credentials;
 
-  // Project configuration.
+  try {
+    credentials = require('grunt-awsebtdeploy-credentials');
+  } catch (err) {
+    credentials = {};
+  }
+
   grunt.initConfig({
     jshint: {
       all: [
@@ -26,7 +31,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Before generating any new files, remove any previously-created files.
     clean: {
       tests: ['tmp']
     },
@@ -35,7 +39,7 @@ module.exports = function (grunt) {
     awsebtdeploy: {
       demo: {
         options: {
-          deployType: 'swapToNew',
+          deployType: 'inPlace',
           accessKeyId: credentials.accessKeyId,
           secretAccessKey: credentials.secretAccessKey,
           region: 'eu-west-1',
@@ -45,63 +49,37 @@ module.exports = function (grunt) {
       }
     },
 
-    // Unit tests.
     nodeunit: {
       tests: ['test/*_test.js']
     }
 
   });
 
-  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('deploy', ['clean', 'createS3Key', 'createSourceBundle', 'awsebtdeploy']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'bumpAppVersion', 'deploy', 'nodeunit']);
+  grunt.registerTask('deploy', ['clean', 'bumpAppVersion', 'createSourceBundle', 'awsebtdeploy']);
+  grunt.registerTask('default', ['jshint', 'deploy', 'nodeunit']);
 
   grunt.registerTask('bumpAppVersion', function () {
-    var packageContents = { version: '0.1.' + new Date().getTime() };
+    var pkg = { version: '0.1.' + new Date().getTime() };
 
-    fs.writeFile('app/package.json', JSON.stringify(packageContents), this.async());
-  });
-
-  grunt.registerTask('createS3Key', function () {
-    var done = this.async();
-
-    exec('git describe --always --dirty=-' + new Date().getTime(), function (err, stdo, stde) {
-      if (err) return done(err);
-
-      grunt.config('s3key', stdo.toString().trim());
-      done();
-    });
+    fs.writeFile('app/package.json', JSON.stringify(pkg), this.async());
   });
 
   grunt.registerTask('createSourceBundle', function () {
-    var done = this.async(),
-        sourceBundle = 'tmp/' + grunt.config('s3key') + '.zip';
+    var zip = new Zip(),
+        sourceBundle = 'tmp/' + require('./app/package.json').version + '.zip';
 
     grunt.file.mkdir('tmp');
 
-    exec('git update-index --no-assume-unchanged package.json && git stash create', {cwd: 'app'}, function (err, stdo) {
-      if (err) return done(err);
+    zip.addLocalFolder('app');
+    zip.writeZip(sourceBundle);
 
-      var stashName = stdo.toString().trim();
-
-      exec('git update-index --assume-unchanged package.json && git archive ' + stashName + ' --format zip -o ../' + sourceBundle, { cwd: 'app' },
-          function (err) {
-            if (err) return done(err);
-
-            grunt.config('awsebtdeploy.demo.options.sourceBundle', sourceBundle);
-            done();
-          });
-    });
+    grunt.config('awsebtdeploy.demo.options.sourceBundle', sourceBundle);
   });
+
 };
